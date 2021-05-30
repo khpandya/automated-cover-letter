e0ddb34302d9127f9c0436e883b242f9896e4cb3
krishna <khpandya@mun.ca>
2021-05-30 17:38:40 -0230

    added reading from excel

--- cover-letter.py
+++ cover-letter.py
@@ -6,6 +6,7 @@ from docx.shared import Pt
 from datetime import date
 from docx2pdf import convert
 import sys
+from openpyxl import load_workbook
 
 def manageDocs(companyName,jobTitle):
     """
@@ -15,8 +16,6 @@ def manageDocs(companyName,jobTitle):
     # remove spaces
     pathCompany=companyName.replace(" ","")
     pathJobTitle=jobTitle.replace(" ","")
-    # change cwd to path of script
-    os.chdir(os.path.dirname(sys.argv[0]))
     # make folder named company name
     newPath=os.path.join(os.getcwd(),pathCompany)
     Path(newPath).mkdir(parents=True,exist_ok=True)
@@ -46,30 +45,63 @@ def replace_string(filename,find,replace):
 def convertToPDF(docxpath):
     convert(docxpath)
 
-companyName=input("company name: ")
-jobTitle=input("job title: ")
-jobId=input("job ID (enter d if no job ID): ")
-contactName=input("contact name (enter d if contact name is unknown): ")
+class XLrow:
+    def __init__(self,companyName,positionName,jobID,contactName):
+        self.companyName=companyName
+        self.positionName=positionName
+        self.jobID=jobID
+        self.contactName=contactName
+    
+    def __str__(self):
+        return str(self.companyName)+" "+str(self.positionName)+" "+str(self.jobID)+" "+str(self.contactName)
 
-if jobId=="d":
-    jobId=""
-else:
-    jobId=" with job id "+jobId
+def readXL():
+    XLrows=[]
+    workbook=load_workbook(filename="Companies.xlsx")
+    sh=workbook.active
+    for rowIdx in range(2, sh.max_row+1):
+        row=[]
+        for colIdx in range(1, sh.max_column+1):
+            cell_obj = sh.cell(row=rowIdx, column=colIdx)
+            row.append(str(cell_obj.value))
+        obj=XLrow(row[0],row[1],row[2],row[3])
+        XLrows.append(obj)
+    return XLrows
 
-if contactName=="d":
-    contactName="Sir/Ma'am"
+def batchGenerate(letterinfo):
+    for row in letterInfo:
+        companyName=row.companyName
+        jobTitle=row.positionName
+        jobId=row.jobID
+        contactName=row.contactName
+
+        if jobId=="d":
+            jobId=""
+        else:
+            jobId=" with job id "+jobId
+
+        if contactName=="d":
+            contactName="Sir/Ma'am"
 
 
-replaceDict={'#companyName#':companyName,
-            '#date#':date.today().strftime("%B %d, %Y"),
-            '#jobTitle#':jobTitle,
-            '#jobId#':jobId,
-            '#contactName#':contactName
-            }
+        replaceDict={'#companyName#':companyName,
+                    '#date#':date.today().strftime("%B %d, %Y"),
+                    '#jobTitle#':jobTitle,
+                    '#jobId#':jobId,
+                    '#contactName#':contactName
+                    }
 
-destination=manageDocs(companyName,jobTitle)
+        destination=manageDocs(companyName,jobTitle)
 
-for find in replaceDict:
-    replace_string(destination,find,replaceDict[find])
+        for find in replaceDict:
+            replace_string(destination,find,replaceDict[find])
 
-convertToPDF(destination)
+        convertToPDF(destination)
+
+
+if __name__ == "__main__":
+    # change cwd to path of script
+    os.chdir(os.path.dirname(sys.argv[0]))
+    letterInfo=readXL()
+    batchGenerate(letterInfo)
+
